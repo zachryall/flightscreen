400560f236835bd7ba054f7ad437b3bd16785b64
Zach Ryall <zach.ryall16@gmail.com>
2025-03-20 21:18:52 +0000

    add basic theme support

--- components/theme.py
+++ components/theme.py
@@ -4,7 +4,10 @@ from rgbmatrix import graphics
 from components import config
 
 font = graphics.Font()
-font.LoadFont(config.config_dict['Display']['font_regular'])
+font.LoadFont(config.config_dict['Theme']['font_regular'])
 
 font_small = graphics.Font()
-font_small.LoadFont(config.config_dict['Display']['font_small'])
+font_small.LoadFont(config.config_dict['Theme']['font_small'])
+
+colour_main = graphics.Color(0, 255, 0)
+colour_accent = graphics.Color(255, 0, 255)
